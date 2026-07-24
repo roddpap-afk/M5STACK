f38006d802cc50238731468f62fcb5ab8806b9d6
roddpap-afk <roddpap@gmail.com>
2026-07-24 19:03:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from datetime import datetime
 import streamlit as st
 
 # Configuração da página para Mobile
-st.set_page_config(page_title="M5Stack Dash", layout="tight")
+st.set_page_config(page_title="M5Stack Dash", layout="wide")
 st.title("📊 Monitoramento M5Stack")
 
 # Botão de atualização manual na interface do celular
@@ -109,5 +109,3 @@ plt.autoscale()
 # --- EXIBIÇÃO NO STREAMLIT (CELULAR) ---
 # Mostra o gráfico adaptando o tamanho dinamicamente para a tela do celular
 st.pyplot(fig, clear_figure=True)
-
-
